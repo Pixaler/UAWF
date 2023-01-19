f24f318997abaa4fe7a278e869405abbccc82bf3
Pixaler <gnidko02@gmail.com>
2023-01-19 12:18:53 +0100

    Clean repo and code

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from prettytable import PrettyTable
 from array import *
 import os
 
+PATH_TO_TXT = "Links.txt"
 
 def get_version_number(app_location):
     """Obtain latest version of app that locate in provided location on Windows"""
@@ -56,134 +57,101 @@ def get_latest_version_techspot(url):
         latest=re.sub(r' ' , "", latest)
     return latest
 
-
-dict_of_prog = {} # Create an empty list of prog
-
-
-# Create/edit files
-
-# How many programs I have 
-count_prog = 0
-amount_of_prog = 0
-
-with open("C:\BATCH\!shit\App\Links.txt") as f:
-    for lines in f:
-        count_prog += 1
-        if count_prog % 4 == 0:
-            amount_of_prog += 1
-    f.close()
-
-amount_of_prog += 2
-
-
-bar = IncrementalBar("Collecting data", max = amount_of_prog)
-count = 1
-
-source = ""
-
-try:
-    with open("Links.txt") as f:
+def amount_of_prog():
+    """Return amount of prog in txt files"""
+    count_prog = 0
+    amount_of_prog = 0
+    with open(PATH_TO_TXT) as f:
         for lines in f:
-            var = str(lines)
-            var = re.sub(r'\n', "", var)
+            count_prog += 1
+            if count_prog % 4 == 0:
+                amount_of_prog += 1
+        f.close()
+    return amount_of_prog
 
-            if var == "GitHub" or var == "PortableApps" or var == "Techspot":
-                source = var
-            else:
-                if count % 4 == 1:
-                    name = var
-            
-                if count % 4 == 2:
-                    url = var
-                    if source == "GitHub":
-                        latest = get_latest_version_github(url)
-                    elif source == "PortableApps":
-                        latest = get_latest_version_portableapps(url)
-                    else:
-                        latest = get_latest_version_techspot(url)
-
-                if count % 4 == 3:
-                    app_location = var
-                    version = get_version_number(app_location)
-
-                if count % 4 == 0:
-                    link = var
-
-                    dict_of_prog[name] = [version, latest, link]
-                    bar.next()
-                count += 1
-    f.close()
-except:
-    print("\n")
-    print("Links.txt are missing")
-
-
-# Harcoded programs that not included in Links.txt
-
-name = "Git"
-url = "https://git-scm.com/downloads"
-soup = soup_reader(url)
-
-for tag in soup.find_all("span", class_="version"):
-   latest=re.sub(r"\n", "", tag.text)
-   latest=re.sub(r" ", "", latest)
-
-app_location = r'C:\PortableApps\Git\git-bash.exe'
-link = "https://git-scm.com/download/win"
-version = get_version_number(app_location)
-
-dict_of_prog[name] = [version, latest, link]
-
-bar.next()
-time.sleep(1)
-
-name = "RivaTuner Statistics Server"
-url="https://www.guru3d.com/files-details/rtss-rivatuner-statistics-server-download.html"
-soup=soup_reader(url)
-for tag in soup.find_all("span", itemprop="itemreviewed"):
-   latest=re.sub(r"Guru3D.*Download ", "", tag.text)
-   latest=re.sub(r" ", "", latest)
-   latest=re.sub(r"build", ".", latest)
-
-app_location = r'C:\PortableApps\RivaTuner Statistics Server\RTSS.exe'
-version = get_version_number(app_location)
-link = "https://www.guru3d.com/files-details/rtss-rivatuner-statistics-server-download.html"
-
-dict_of_prog[name] = [version, latest, link]
-
-bar.next()
-time.sleep
-
-
-bar.finish() 
-time.sleep(1)
-
-# Creation of table
-os.system('cls')
-th=['Name','Installed','Latest']
-table = PrettyTable(th) 
-
-for name_list in dict_of_prog:
-    name_tb = name_list
-    version_tb = dict_of_prog[name_list][0]
-    latest_tb = dict_of_prog[name_list][1]
-    table.add_row([name_tb, version_tb, latest_tb])  # Создаем строку с нашими данными
-
-print(table)
-
-# Menu with download links
-while True:
-    print(".\n.")
-    menu_option = input(f"Type name of program. Type (exit) to stop: ")
-    if menu_option == "exit":
-        break
+def get_dict(bar):
     try:
-        # Search in dictionary name of program. Make name less sensitive to registry
-        for name in dict_of_prog:
-            if name.lower() == menu_option.lower():
-                down_link = dict_of_prog[name][2]
-        webbrowser.open(down_link, new = 0, autoraise = True)
-        print(".\n.")
+        dict_of_prog = {}
+        count = 1
+        with open(PATH_TO_TXT) as f:
+            for lines in f:
+                var = str(lines)
+                var = re.sub(r'\n', "", var)
+
+                if var == "GitHub" or var == "PortableApps" or var == "Techspot":
+                    source = var
+                else:
+                    if count % 4 == 1:
+                        name = var
+                
+                    if count % 4 == 2:
+                        url = var
+                        if source == "GitHub":
+                            latest = get_latest_version_github(url)
+                        elif source == "PortableApps":
+                            latest = get_latest_version_portableapps(url)
+                        else:
+                            latest = get_latest_version_techspot(url)
+
+                    if count % 4 == 3:
+                        app_location = var
+                        version = get_version_number(app_location)
+
+                    if count % 4 == 0:
+                        link = var
+
+                        dict_of_prog[name] = [version, latest, link]
+                        bar.next()
+                    count += 1
+        f.close()
+        return dict_of_prog
     except:
+        print("\n")
+        print("Links.txt are missing")
+
+def create_table(dict_of_prog):
+    os.system('cls')
+    th=['Name','Installed','Latest']
+    table = PrettyTable(th) 
+
+    for name_list in dict_of_prog:
+        name_tb = name_list
+        version_tb = dict_of_prog[name_list][0]
+        latest_tb = dict_of_prog[name_list][1]
+        table.add_row([name_tb, version_tb, latest_tb])  # Создаем строку с нашими данными
+
+    print(table)
+
+def main():
+    # Create/edit files
+
+    amount = amount_of_prog() # Amount of program
+
+    bar = IncrementalBar("Collecting data", max = amount)
+
+    dict_of_prog = get_dict(bar)# Create a list of prog
+
+    bar.finish() 
+    time.sleep(1)
+
+    create_table(dict_of_prog) # Creation of table
+
+    # Menu with download links
+    while True:
         print(".\n.")
-        print(f"Program {menu_option} not in Links.txt. Please add this program or type program that printed in table.")
+        menu_option = input(f"Type name of program. Type (exit) to stop: ")
+        if menu_option == "exit":
+            break
+        try:
+            # Search in dictionary name of program. Make name less sensitive to registry
+            for name in dict_of_prog:
+                if name.lower() == menu_option.lower():
+                    down_link = dict_of_prog[name][2]
+            webbrowser.open(down_link, new = 0, autoraise = True)
+            print(".\n.")
+        except:
+            print(".\n.")
+            print(f"Program {menu_option} not in Links.txt. Please add this program or type program that printed in table.")
+
+if __name__ == "__main__":
+    main()
