a1827f951d4ee3cfc88eb56956c685239ab46def
Pixaler <gnidko02@gmail.com>
2026-09-25 12:24:29 +0200

    Change from dictionary to class list

--- output_processor.py
+++ output_processor.py
@@ -13,16 +13,16 @@ class OutputProcessor:
         table = PrettyTable(th) 
 
         for dict_var in list_of_prog:
-            index_tb = dict_var["index"]+1
-            name_tb = dict_var["name"]
-            version_tb = dict_var["version"]
-            latest_tb = dict_var["latest"]
+            index_tb = list_of_prog.index(dict_var)+1
+            name_tb = dict_var.name
+            version_tb = dict_var.current_version
+            latest_tb = dict_var.latest_version
             table.add_row([index_tb, name_tb, version_tb, latest_tb])  # Создаем строку с нашими данными
         print(table)
     
     def program_in_dict(self, list_of_prog, user_choice):
         """Check user input in list of dictionary"""
 
-        down_link = list_of_prog[user_choice-1]["download_link"]
+        down_link = list_of_prog[user_choice-1].download_link
         webbrowser.open(down_link, new=0, autoraise = True)
         return True
